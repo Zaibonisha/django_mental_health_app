9260ad155ab78bc6795e69bb08487d5d8b9c047e
Zaibonisha Mohamed <zsmohamed94@gmail.com>
2024-12-08 16:25:01 +0200

    changes made in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ["*"]
+
 
 # Application definition
 INSTALLED_APPS = [
